e02021ae6aae91e0b373c5079c9448eaa79f5aaa
Fernanda <fernandacorredera@gmail.com>
2023-01-15 14:29:55 -0300

    add butto

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function funcao1(click)
+
+            {
+                var x, id="click";
+                var r=confirm("Thank you! We've added your card details");
+                if (r==true)
+                {
+                    x=""
+                }
+                else {
+                    x=""
+                }
+                document.getElementById("click").innerHTML=x;
+            }
